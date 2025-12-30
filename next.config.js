9ec4b4faa4c60744f4d1d5287a8b2e9d46f71709
Pedro Teixeira <pedrops02@gmail.com>
2025-12-29 23:19:38 -0300

    feat: update remotePatterns in next.config.js for Supabase Storage

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "dfcjflhdcnybigvwdojc.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
